2e4c0b1fbb23ab9f94a6c7709ebb6914588ef0f1
Yann ILAS <yann.ilas@gmail.com>
2025-11-15 18:43:02 +0100

    Initial version

--- ch1/hello.go
+++ ch1/hello.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world !")
+	fmt.Println("Bonjour", "Tutu", "toto")
+	fmt.Println("Hello World...")
+	fmt.Printf("Hello %s !\n", "world")
 }
